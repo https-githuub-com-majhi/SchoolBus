27a13607d1e7d2123c89a0a1faffed6212c6640c
prak899 <majhi9442@gmail.com>
2022-06-16 13:40:38 +0530

    Initial commit

---
+++ app/src/main/java/com/siddhi/software/MainActivity.kt
@@ -0,0 +1,188 @@
+package com.siddhi.software
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.KeyEvent
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private var progress: Progress? = null
+    private var isLoaded: Boolean = false
+    private var doubleBackToExitPressedOnce = false
+    private var webURL = "https://myschoolbusinfo.in/" // Change it with your URL
+
+    private lateinit var webView: WebView
+    private lateinit var infoTV: TextView
+    private lateinit var rootView: ConstraintLayout
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //init of views
+        webView = findViewById(R.id.webView)
+        infoTV = findViewById(R.id.infoTV)
+        rootView = findViewById(R.id.rootView)
+
+        //code start here
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.allowFileAccess = true
+        settings.domStorageEnabled = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.supportMultipleWindows()
+        progress = Progress(this, R.string.please_wait, cancelable = true)
+        if (!isOnline()) {
+            showToast(getString(R.string.no_internet))
+            infoTV.text = getString(R.string.no_internet)
+            showNoNetSnackBar()
+            return
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onResume() {
+        if (isOnline() && !isLoaded) loadWebView()
+        super.onResume()
+    }
+
+    private fun loadWebView() {
+        showProgress(true)
+        infoTV.text = ""
+        webView.loadUrl(webURL)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url.toString()
+                if (url.startsWith("tel:") || url.startsWith("whatsapp:")) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                    webView.goBack()
+                    return true
+                } else {
+                    view?.loadUrl(url)
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                showProgress(true)
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                isLoaded = true
+                showProgress(false)
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                isLoaded = false
+                val errorMessage = "Got Error! $error"
+                showToast(errorMessage)
+                infoTV.text = errorMessage
+                showProgress(false)
+                super.onReceivedError(view, request, error)
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    showToastToExit()
+                }
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun showToastToExit() {
+        when {
+            doubleBackToExitPressedOnce -> {
+                onBackPressed()
+            }
+            else -> {
+                doubleBackToExitPressedOnce = true
+                showToast(getString(R.string.back_again_to_exit))
+                Handler(Looper.myLooper()!!).postDelayed(
+                    { doubleBackToExitPressedOnce = false },
+                    2000
+                )
+            }
+        }
+    }
+
+    private fun showProgress(visible: Boolean) {
+        progress?.apply { if (visible) show() else dismiss() }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isOnline(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showNoNetSnackBar() {
+        val snack =
+            Snackbar.make(rootView, getString(R.string.no_internet), Snackbar.LENGTH_INDEFINITE)
+        snack.setAction(getString(R.string.settings)) {
+            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        }
+        snack.show()
+    }
+}
